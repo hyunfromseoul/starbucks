7abffd18deab8234f44ac019ff4d96e3f292bcde
jayfromseoul <76645966+jayfromseoul@users.noreply.github.com>
2021-06-07 23:36:55 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
 const search = document.querySelector('.search'),
     searchInput = document.querySelector('.search-input'),
+    dropdownContainer = document.querySelector('.dropdown-container'),
     coffee = document.querySelector('.nav-coffee'),
-    coffeeDropDown = document.querySelector('.drop-1')
+    coffeeDropdown = document.querySelector('.drop-1'),
+    menu = document.querySelector('.nav-menu'),
+    menuDropdown = document.querySelector('.drop-2')
 
 
-// 검색 애니메이션
+// 검색 이벤트리스너
 search.addEventListener('click', ()=> {
     searchInput.classList.add('active')
-    console.log('here')
 })
 
-coffee.addEventListener('mouseover', ()=> {
-    coffeeDropDown.classList.add('active')
+
+// dropdown 이벤트리스너
+coffee.addEventListener('mouseenter', ()=> {
+    // 다른 dropdown active를 지움
+    menuDropdown.classList.remove('active')
+
+    coffeeDropdown.classList.add('active')
+})
+coffeeDropdown.addEventListener('mouseleave', ()=> {
+    coffeeDropdown.classList.remove('active')
 })
 
-coffee.addEventListener('mouseout', ()=> {
-    coffeeDropDown.classList.remove('active')
+menu.addEventListener('mouseenter', ()=> {
+    // 다른 dropdown active를 지움
+    coffeeDropdown.classList.remove('active')
+
+    menuDropdown.classList.add('active')
+})
+menuDropdown.addEventListener('mouseleave', ()=> {
+    menuDropdown.classList.remove('active')
 })
